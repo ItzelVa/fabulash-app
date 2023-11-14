9dff24d88bb82951ea0d32ccccdcb94171fdf51a
ItzelVa <itzelochoavallejo@gmail.com>
2023-11-13 21:08:07 -0600

    Modificaciones a Mis Citas y creación de Programar

--- app/src/main/java/com/itzel/fabulash/models/DataMyAppointments.kt
+++ app/src/main/java/com/itzel/fabulash/models/DataMyAppointments.kt
@@ -1,3 +1,3 @@
 package com.itzel.fabulash.models
 
-data class DataMyAppointments(val status:String, val fecha:String, val hora:String, val empleado:String, val servicio:String, val imgEmpleado:String)
+data class DataMyAppointments(var status:String, val fecha:String, val hora:String, val empleado:String, val servicio:String, val imgEmpleado:String)
